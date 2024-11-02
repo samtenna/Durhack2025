b26812b2bdbd7ad3d1c2f167be995e8a921b8227
BingBlingBoy <ckeiranbarnes@gmail.com>
2024-11-02 14:16:37 +0000

    Navbar and footer design

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,12 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
       colors: {
         "Primary":"#9e8862",
         "Secondary":"#a8c5a1",
         "Acccent":"#8cb898"
       }
+    },
   },
   plugins: [],
 }
