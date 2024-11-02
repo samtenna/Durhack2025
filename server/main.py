583c6063cc875e1dc087b4fd4a85cfdab78b9e0c
BingBlingBoy <ckeiranbarnes@gmail.com>
2024-11-02 16:14:34 +0000

    HTMX WORKS OMG

--- server/main.py
+++ server/main.py
@@ -4,6 +4,19 @@ from flask_htmx import HTMX
 app = Flask(__name__)
 htmx = HTMX(app)
 
-@app.route('/')
-def hello_world():
+@app.route("/")
+def index():
     return render_template("index.html")
+
+from flask_htmx import make_response
+
+@app.route("/hola-mundo")
+def hola_mundo():
+    body = "Hola Mundo!"
+    return make_response(
+        body,
+        push_url=False,
+        trigger={"event1": "A message", "event2": "Another message"},
+    )
+
+app.run(debug=True)
