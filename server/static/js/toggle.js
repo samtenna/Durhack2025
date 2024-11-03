efb5f2087f3b5fcb7e180c98f8e6ec8290db1fbd
BingBlingBoy <ckeiranbarnes@gmail.com>
2024-11-03 02:54:40 +0000

    fix bugs

--- server/static/js/toggle.js
+++ server/static/js/toggle.js
@@ -1,4 +1,9 @@
 const Toggle = (id) => {
+    let cl = document.querySelectorAll(".textbox")
+    cl.forEach(element => {
+        element.innerHTML = ""
+    })
+    
     const content = document.getElementById(`${id}-textbox`);
     if (content.innerHTML) {
         content.innerHTML = "";
